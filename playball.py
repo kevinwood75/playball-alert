2a10a23295f5d996f5ca98d68f7cba6e25763e11
kevinwood75 <kevin.wood75@gmail.com>
2018-07-07 07:41:00 -0400

    added logic

--- playball.py
+++ playball.py
@@ -5,20 +5,30 @@ import time
 import datetime
 
 
-def goal_light():
-    for line in range(15):
+def goal_light(green,red,blue,count):
+    for line in range(count):
         bulb = Bulb("192.168.2.93")
         bulb.set_brightness(100)
-        bulb.set_rgb(0, 0, 255)
-        bulb.turn_on(effect="smooth", duration=50)
-        time.sleep(1)
+        bulb.turn_on()
+        bulb.set_rgb(green, red, blue)
+        time.sleep(3)
         bulb.turn_off()
         time.sleep(1)
 
-
 def logger(line,datestr):
     with open("playball.log", "a") as ballfile:
-        ballfile.write("{0},{1}".format(datestr,line))
+        ballfile.write("{0},{1}\n".format(datestr,line))
+
+
+def check_winning(hruns,aruns,datestr):
+    if int(aruns) < int(hruns):
+       goal_light(255,0,0,3)
+    else:
+       goal_light(0,255,0,3)
+
+    line = "{0},{1}".format(hruns,aruns)
+    logger(line,datestr)
+
 
 
 def play_ball():
@@ -32,22 +42,36 @@ def play_ball():
         current_minute = now.minute
         datestr = "{0}-{1}-{2}-{3}:{4}".format(current_year,current_month,current_day,current_hour,current_minute)
         day = mlbgame.day(current_year, current_month, current_day, away='Blue Jays', home='Blue Jays')
-        game = day[0]
-        if int(game.home_team_runs) > run_history:
-            logger(datestr,game.home_team_runs)
-            run_history = game.home_team_runs
-            try:
-               goal_light()
-            except main.BulbException:
-               time.sleep(5)
-               goal_light()
-        if int(current_hour) == 5:
-            run_history = 0
-        time.sleep(5)
+        if len(day) > 0:
+           game = day[0]
+           if 'Blue Jays' in game.away_team:
+               if int(game.away_team_runs) > run_history:
+                  logger(datestr,game.away_team_runs)
+                  run_history = game.away_team_runs
+                  try:
+                     goal_light(0,0,255,5)
+                  except main.BulbException:
+                     time.sleep(5)
+                     goal_light(0,0,255,5)
+                  check_winning(game.home_team_runs,game.away_team_runs,datestr) 
+           else:
+               if int(game.home_team_runs) > run_history:
+                  logger(datestr,game.home_team_runs)
+                  run_history = game.home_team_runs
+                  try:
+                     goal_light(0,0,255,5)
+                  except main.BulbException:
+                     time.sleep(5)
+                     goal_light(0,0,255,5)
+                  check_winning(game.away_team_runs,game.home_team_runs,datestr)
+
+           if int(current_hour) == 10:
+               print("hello")
+               run_history = 0
+               logger(datestr,"reset")
+               print("{0},{1}\n".format("reset",run_history))
+           time.sleep(1)
 
 
 if __name__ == "__main__":
     play_ball()
-
-
-
